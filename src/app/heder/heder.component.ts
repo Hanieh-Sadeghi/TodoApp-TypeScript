05a15d56c1c44e441948ac96049d5a663bfe94a9
Hanieh-Sadeghi <haniehsadeghi2@outlook.com>
2023-11-29 16:33:07 +0330

    componet header

---
+++ src/app/heder/heder.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-heder',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './heder.component.html',
+  styleUrl: './heder.component.css'
+})
+export class HederComponent {
+
+}
