f36f9fd99f683284a116d348fa14d2491b03d2c0
Hanieh-Sadeghi <haniehsadeghi2@outlook.com>
2023-11-29 17:56:23 +0330

    add comonet  li

---
+++ src/app/li/li.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-li',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './li.component.html',
+  styleUrl: './li.component.css'
+})
+export class LiComponent {
+
+}
