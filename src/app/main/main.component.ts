6c4faad336950c8a764dce6ab5b2748914290566
Hanieh-Sadeghi <haniehsadeghi2@outlook.com>
2023-11-30 12:43:39 +0330

    add import main

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { UlComponent } from "../ul/ul.component";
+import { ButtonsComponent } from "../buttons/buttons.component";
 
 @Component({
-  selector: 'app-main',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './main.component.html',
-  styleUrl: './main.component.css'
+    selector: 'app-main',
+    standalone: true,
+    templateUrl: './main.component.html',
+    styleUrl: './main.component.css',
+    imports: [CommonModule, UlComponent, ButtonsComponent]
 })
 export class MainComponent {
 
