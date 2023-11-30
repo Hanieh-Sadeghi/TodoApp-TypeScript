8cb52ef9b12b5314639a48c87b2f33edea41c572
Hanieh-Sadeghi <haniehsadeghi2@outlook.com>
2023-11-30 17:08:58 +0330

    add componet li ul

--- src/app/li/li.component.ts
+++ src/app/li/li.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -6,9 +6,22 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './li.component.html',
-  styles: [
-    '@import "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"',
-  ],
-  styleUrl: './li.component.css',
+  styles: '@import "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"',
+  styleUrls: ['./li.component.css'],
 })
-export class LiComponent {}
+
+export class LiComponent {
+  @Input() task: string = '';
+
+  @Output() checkBoxEvent = new EventEmitter<boolean>();
+  @Output() editEvent = new EventEmitter<string>();
+
+
+  test(check: boolean) {
+    console.log(check);
+  }
+
+  editButton(todo: string){
+    console.log(todo)
+  }
+}
