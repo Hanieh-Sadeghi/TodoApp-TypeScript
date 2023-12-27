3008df71c0b471dbfa0bb0a671dba03ca2c76706
Hanieh-Sadeghi <haniehsadeghi2@outlook.com>
2023-12-27 17:29:17 +0330

    add Output

--- src/app/buttons/buttons.component.ts
+++ src/app/buttons/buttons.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component , Input , Output , EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -9,5 +9,11 @@ import { CommonModule } from '@angular/common';
   styleUrl: './buttons.component.css'
 })
 export class ButtonsComponent {
+  @Output() allButtonEvent = new EventEmitter<boolean> ();
+  @Output() activeButtonEvent = new EventEmitter<string> ();
+  @Output() completeButtonEvent = new EventEmitter<string> ();
+  @Output() signUpButtonEvent = new EventEmitter<string> ();
+  @Output() loginButtonEvent = new EventEmitter<string> ();
+
 
 }
