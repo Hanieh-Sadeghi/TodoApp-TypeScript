fd4d32f1a849780cfbccf018643118a970e56034
Hanieh-Sadeghi <haniehsadeghi2@outlook.com>
2023-12-27 17:21:22 +0330

    add

--- src/app/li/li.component.ts
+++ src/app/li/li.component.ts
@@ -6,7 +6,7 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './li.component.html',
-  // styles: '@import "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"',
+  styles: '@import "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"',
   styleUrls: ['./li.component.css'],
 })
 
@@ -22,7 +22,7 @@ export class LiComponent {
   }
 
   editTask(task: string) {
-    this.editEvent.emit(task);
+    this.editEvent.emit(this.task);
   }
 
   deleteTask() {
