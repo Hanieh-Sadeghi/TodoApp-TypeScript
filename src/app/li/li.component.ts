370e5edbe4a9cf2a36945ab73272c03d52b7fb7d
Hanieh-Sadeghi <haniehsadeghi2@outlook.com>
2023-12-30 17:54:26 +0330

    add inter Face

--- src/app/li/li.component.ts
+++ src/app/li/li.component.ts
@@ -1,25 +1,33 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+interface checkBoxEvent {
+  value: boolean;
+  task: string;
+}
+
 @Component({
   selector: 'app-li',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './li.component.html',
-  styles:
-    '@import "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"',
+  // styles:
+  //   '@import "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"',
   styleUrls: ['./li.component.css'],
 })
-
 export class LiComponent {
   @Input() task: string = '';
 
-  @Output() checkBoxEvent = new EventEmitter<boolean>();
+  @Output() checkBoxEvent = new EventEmitter<checkBoxEvent>();
   @Output() editEvent = new EventEmitter<string>();
   @Output() deleteEvent = new EventEmitter<string>();
 
-  test(check: boolean) {
-    this.checkBoxEvent.emit(check);
+  test(check: boolean, task: string) {
+    let event: checkBoxEvent = {
+      value: check,
+      task: task,
+    };
+    this.checkBoxEvent.emit(event);
   }
 
   editTask(task: string) {
